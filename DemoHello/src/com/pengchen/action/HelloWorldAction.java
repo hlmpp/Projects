9a043b0948dd770b231a6e7ed5750084783cd664
penchen <chen.peng@pengyunnetwork.com>
2018-04-01 09:45:09 +0800

    for method call

--- DemoHello/src/com/pengchen/action/HelloWorldAction.java
+++ DemoHello/src/com/pengchen/action/HelloWorldAction.java
@@ -3,7 +3,16 @@ package com.pengchen.action;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class HelloWorldAction extends ActionSupport {
-
+	
+	
+	public String add() {
+		return SUCCESS;
+	}
+	
+	public String update() {
+		return SUCCESS;
+	}
+	
 	@Override
 	public String execute() throws Exception {
 		
