f5faaf4e2bce0afb173c67e959769e5db47752cb
yangtzeshore <772307865@qq.com>
2020-05-26 17:11:40 +0800

    session结合拦截器实现了认证和授权

--- securityspringmvc/src/main/java/com/pengchen/security/springmvc/config/WebConfig.java
+++ securityspringmvc/src/main/java/com/pengchen/security/springmvc/config/WebConfig.java
@@ -1,11 +1,14 @@
 package com.pengchen.security.springmvc.config;
 
+import com.pengchen.security.springmvc.interceptor.SimpleAuthenticationInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -19,6 +22,9 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
         includeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, value = Controller.class)})
 public class WebConfig implements WebMvcConfigurer {
 
+    @Autowired
+    SimpleAuthenticationInterceptor simpleAuthenticationInterceptor;
+
     @Bean
     public InternalResourceViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -31,5 +37,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("login");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(simpleAuthenticationInterceptor).addPathPatterns("/r/**");
     }
 }
